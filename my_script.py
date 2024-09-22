170da09a3c00cbc2e91026286cd25574c08f4e65
rlugog <rlugog@gmail.com>
2024-09-22 09:22:50 -0600

    Added new project files and configured line endings

---
+++ my_script.py
@@ -0,0 +1,11 @@
+# print("This is my first python script written in VS CODE")
+
+import os
+
+print("My working directory is {os.getcwd()}")
+
+x= 10
+if x % 2 == 0:
+    print(f"{x} is an even number")
+else:
+    print(f"{x} is an odd number")
